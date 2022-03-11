95e84a87afc024afbc44315118d87d5965c2ece5
Shaun van Ardenne <s.vanardenne@gmail.com>
2022-03-10 20:51:08 -0800

    Added mongodb connection

--- api/app.js
+++ api/app.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const app = express();
-const PORT = 5000;
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
-app.listen(PORT, () => {
-  console.log("Server is running on port", PORT);
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.on("connected", () => {
+  console.log("Connected to DB");
+});
+mongoose.connection.on("error", (err) => {
+  console.log("Error connecting to DB", err);
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Server is running on port", process.env.PORT);
 });
