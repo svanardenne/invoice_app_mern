9a0a57e5bb31c4d7b3f690e017a5e21d8b5b73a1
Shaun van Ardenne <s.vanardenne@gmail.com>
2022-03-10 22:53:09 -0800

    Added json middleware and basic user model

--- api/app.js
+++ api/app.js
@@ -1,9 +1,11 @@
+// Global Imports
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 
+// Mongoose connection
 mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on("connected", () => {
   console.log("Connected to DB");
@@ -12,6 +14,14 @@ mongoose.connection.on("error", (err) => {
   console.log("Error connecting to DB", err);
 });
 
+// Models
+
+// Middleware
+app.use(express.json());
+
+// Routes
+
+// Server setup
 app.listen(process.env.PORT, () => {
   console.log("Server is running on port", process.env.PORT);
 });
