38a62573794bf6d0bbf0eafc8a74eafa9b8e7db0
Shaun van Ardenne <s.vanardenne@gmail.com>
2022-03-22 23:20:06 -0700

    Added layout file

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 
+import Layout from "./components/Layout";
+
 const Routes = () => {
-  return <h1>I am stuff!</h1>;
+  return (
+    <BrowserRouter>
+      <Layout />
+    </BrowserRouter>
+  );
 };
 
 export default Routes;
---
+++ client/src/components/Layout.js
@@ -0,0 +1,5 @@
+const Layout = () => {
+  return <h1>Hello World!</h1>;
+};
+
+export default Layout;
