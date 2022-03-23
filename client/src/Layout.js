a093b20e2d0d4f557294480d184cfc2c5d8ab0e5
Shaun van Ardenne <s.vanardenne@gmail.com>
2022-03-23 16:24:53 -0700

    Added Basic navbar layout and created signed-out home page jumbotron

--- client/src/Layout.js
+++ client/src/Layout.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Container } from "react-bootstrap";
 
 import Navbar from "./components/Navbar";
@@ -8,7 +9,18 @@ const Layout = () => {
     <React.Fragment>
       <Navbar />
       <Container>
-        <h1>I am the body!</h1>
+        <div className="jumbotron row justify-content-center">
+          <div className="center">
+            <h1>Welcome to the Invoice App</h1>
+            <h2>Sign up to stary using the App!</h2>
+          </div>
+          <Link
+            className="btn btn-lg btn-primary col-md-3 col-lg-2"
+            to="/signup"
+          >
+            Sign Up!
+          </Link>
+        </div>
       </Container>
     </React.Fragment>
   );
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,26 +1,28 @@
 import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 const NavMenu = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <LinkContainer to="/">
+          <Navbar.Brand>Invoice App</Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <LinkContainer to="/">
+              <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/help">
+              <Nav.Link>Help</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/contact">
+              <Nav.Link>Contact</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <Nav.Link>Login</Nav.Link>
+            </LinkContainer>
           </Nav>
         </Navbar.Collapse>
       </Container>
