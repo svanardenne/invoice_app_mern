8a46bd58a0fd1557ea07e272670ae159b7cf4ea0
Shaun van Ardenne <s.vanardenne@gmail.com>
2022-04-22 11:00:08 -0700

    Finished adding basic routes

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,9 +1,14 @@
+// Global Imports
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+// Element Imports
 import Layout from "./Layout";
 import SignedOutHome from "./pages/SignedOutHome";
 import Help from "./pages/Help";
+import Contact from "./pages/Contact";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
 const Router = () => {
   return (
@@ -12,6 +17,9 @@ const Router = () => {
       <Routes>
         <Route exact path="/" element={<SignedOutHome />} />
         <Route path="/help" element={<Help />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
