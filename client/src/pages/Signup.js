fd16bc4f3ca15a6887d5d7368b893ea310247082
Shaun van Ardenne <s.vanardenne@gmail.com>
2022-04-22 17:15:09 -0700

    Created basic sign up form html

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,8 +1,30 @@
+import { Container, Form, Button } from "react-bootstrap";
+
 const Signup = () => {
   return (
-    <div>
-      <h1>Signup Page</h1>
-    </div>
+    <Container>
+      <Form>
+        <Form.Group className="mb-3" controlId="name-group">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="text" placeholder="Enter Name" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="email-group">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="text" placeholder="Enter Email" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="password-group">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Enter Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="confirm-password-group">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control type="password" placeholder="Confirm Password" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Sign Up
+        </Button>
+      </Form>
+    </Container>
   );
 };
 
